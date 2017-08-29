54eb97d41a853ab4759f76854d489fd4dbb7b7fc
Liu Zongyuan <liuzongyuan0505@gmail.com>
2017-08-29 19:50:34 +0800

    创建项目。

---
+++ src/test/testMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author lzy05
+ *
+ */
+public class testMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello VCampus!");
+	}
+
+}
