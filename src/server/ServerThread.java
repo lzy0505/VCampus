30eff8f5398cc4918e37054e278aa5b8236cdafc
Stormuzi <3263707107@qq.com>
2017-09-03 13:28:54 +0800

    安排了一下服务端情况，客户端和GUI有待完善

--- src/server/ServerThread.java
+++ src/server/ServerThread.java
@@ -6,6 +6,7 @@ package server;
 import java.awt.Choice;
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import utils.User;
@@ -20,6 +21,7 @@ public class ServerThread implements Runnable{
 	private Socket socket=null;
 	private ObjectInputStream sois = null;
 	private ObjectOutputStream soos = null;
+	private DataBase db=null;
 	
 	public ServerThread(Socket socket) {
 		this.socket = socket;
@@ -29,23 +31,53 @@ public class ServerThread implements Runnable{
 	
 	public void run() {
 			try {
+				db=new DataBase();
+				db.connectToDB();
 				sois = new ObjectInputStream(socket.getInputStream());
 				soos = new ObjectOutputStream(socket.getOutputStream());
+				//a is used to send message 
 				HashMap<String,String> a =new HashMap<String,String>();
+				//b is used to get message
 				HashMap<String,String> b =new HashMap<String,String>();
 				b = (HashMap<String,String>) sois.readObject();
-				
-				if(b.get("username").equals("qwe")) {
-					a.put("result", "success");
-				}else {
-					a.put("result", "fault");
+				String op = b.get("op");
+				//a HashMap array
+				ArrayList<HashMap<String,String>> aList=null;
+				switch (op) {
+				case "sign in":
+					//get a array of HashMap whose username equal to b'username
+					aList=db.selectWhere("user", "username = "+b.get("username"));
+					if(aList.size()==0) {
+						a.put("result", "fail");
+						a.put("reason", "Username or Password is false!");
+						break;
+					}
+					else if (aList.get(0).get("password").equals(b.get("password"))) {
+						a.put("result", "success");
+						break;
+					}
+					else {
+						a.put("result", "fail");
+						a.put("reason", "Username or Password is false!");
+						break;
+					}
+					
+				case "sign up":
+					aList=db.selectWhere("user", "username = "+b.get("username"));
+					// if there's no username same as b'username,which means sing up is allowable;
+					if(aList.size()==0) {
+						b.remove("op");
+						db.insert("user",b );
+						a.put("result", "success");
+						break;
+					}
+					else {
+						a.put("result","fail");
+						a.put("reason","Username has been used!");
+						break;
+					}
 				}
 				soos.writeObject(a);
-				
-				
-				/*
-				 * 接受到指令后，做出响应操作这里有待完善！！！！！！！！
-				 */
 				
 			} catch (IOException e) {
 				// TODO 自动生成的 catch 块
@@ -55,8 +87,6 @@ public class ServerThread implements Runnable{
 				e.printStackTrace();
 			}
 	
-
-		
 	}
 	
 
